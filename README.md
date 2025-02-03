33c82e7db7b9d53a5216d9b4133747907ebff886
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-02 21:18:15 -0500

    support debugging
    Uses the 1Password npm package rather than the CLI.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ```sh
 npm install
-op run --env-file=.env npx tsx index.ts
+npx tsx index.ts
 ```
--- index.ts
+++ index.ts
@@ -5,36 +5,40 @@ import freshbooks from "@freshbooks/api";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import open from "open";
+import { read as opRead } from "@1password/op-js";
 
 const { Client } = freshbooks;
 
-const clientId = process.env.FRESHBOOKS_APPLICATION_CLIENT_ID;
-const clientSecret = process.env.FRESHBOOKS_APPLICATION_CLIENT_SECRET;
+const clientId = opRead.parse(
+  "op://v7ogqjxnttwfv527qvfjpf7fcq/FreshBooks/API/Client ID"
+);
+const clientSecret = opRead.parse(
+  "op://v7ogqjxnttwfv527qvfjpf7fcq/FreshBooks/API/Client Secret"
+);
 
 if (!clientId || !clientSecret) {
   throw new Error("Missing FreshBooks application client ID or client secret");
 }
 
-// Instantiate new FreshBooks API client
 const client = new Client(clientId, {
   clientSecret,
   redirectUri: "https://afeld.me",
 });
 
-// Give this URL to the user so they can authorize your application
-const authUrl = client.getAuthRequestUrl();
-// console.log(authUrl);
+const authorize = async () => {
+  const authUrl = client.getAuthRequestUrl([
+    "user:profile:read",
+    "user:clients:read",
+    "user:time_entries:read",
+    "user:time_entries:write",
+  ]);
+  await open(authUrl, { app: { name: "firefox" } });
 
-await open(authUrl, { app: { name: "firefox" } });
+  const rl = readline.createInterface({ input, output });
+  const code = await rl.question("Enter the authorization code: ");
+  rl.close();
 
-const rl = readline.createInterface({ input, output });
-const answer = await rl.question("Enter the authorization code: ");
-rl.close();
+  await client.getAccessToken(code);
+};
 
-// This will redirect them to https://your-redirect-uri.com/?code=XXX
-// const code = ...
-
-// Returns an object containing the access token, refresh token, and expiry date
-// Note that this function sets the token on this client instance to automatically
-// authenticates all future requests with this client instance
-// const tokens = client.getAccessToken(code);
+await authorize();
